33fbd83406671933eba9a32825892c6f70cff48d
pablo.kraan <pablo.kraan@gmail.com>
2015-09-17 21:54:16 -0300

    OSGi v2: lots of changes to have  a simple core app working with core + http connector
    _ Updating servlet API version
    _ Adding bundle information
    _ Some classloading fixes
    _ Many TODOs pending

--- core/src/main/java/org/mule/context/DefaultMuleContextBuilder.java
+++ core/src/main/java/org/mule/context/DefaultMuleContextBuilder.java
@@ -112,7 +112,8 @@ public class DefaultMuleContextBuilder implements MuleContextBuilder
 
         muleContext.setLocalMuleClient(new DefaultLocalMuleClient(muleContext));
         muleContext.setExceptionListener(new DefaultSystemExceptionStrategy(muleContext));
-        muleContext.setExecutionClassLoader(Thread.currentThread().getContextClassLoader());
+        //TODO(pablo.kraan): OSGi - this is wrong - context classLoader is the root app classlodear conating System + OSGi classes only
+        //muleContext.setExecutionClassLoader(Thread.currentThread().getContextClassLoader());
 
         JavaObjectSerializer defaultObjectSerializer = new JavaObjectSerializer();
         defaultObjectSerializer.setMuleContext(muleContext);
